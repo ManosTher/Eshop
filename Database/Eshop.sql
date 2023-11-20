aa96f6977ef1f387f7185cca0fb89bf2f65995bc
ManosTher <emmtherap@gmail.com>
2023-11-20 20:14:31 +0200

    DB update

--- Database/Eshop.sql
+++ Database/Eshop.sql
@@ -1,168 +1,102 @@
--- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: eshop
--- ------------------------------------------------------
--- Server version	8.0.33
+-- Create the database
+CREATE DATABASE eshop;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Switch to the database
+USE eshop;
 
---
--- Table structure for table `category`
---
+-- Table: Users
+CREATE TABLE Users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  firstName VARCHAR(255) NOT NULL,
+  lastName VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  city VARCHAR(255),
+  address VARCHAR(255),
+  points INT,
+  password VARCHAR(255) NOT NULL
+);
 
-DROP TABLE IF EXISTS `category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `category` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `Categ` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Table: Guest
+CREATE TABLE Guest (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  firstName VARCHAR(255) NOT NULL,
+  lastName VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  city VARCHAR(255),
+  address VARCHAR(255),
+  pswd VARCHAR (255)
+);
 
---
--- Dumping data for table `category`
---
+-- Table: Category
+CREATE TABLE Category (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  Categ VARCHAR(255) NOT NULL
+);
 
-LOCK TABLES `category` WRITE;
-/*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'Skincare'),(2,'Makeup'),(3,'Haircare'),(4,'Fragrance'),(5,'Body Care');
-/*!40000 ALTER TABLE `category` ENABLE KEYS */;
-UNLOCK TABLES;
+-- Table: Products
+CREATE TABLE Products (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  productName VARCHAR(255) NOT NULL,
+  descrip TEXT,
+  quantity INT,
+  price DECIMAL(10, 2),
+  idCategory INT,
+  FOREIGN KEY (idCategory) REFERENCES Category(id)
+);
 
---
--- Table structure for table `guest`
---
+-- Table: Orders
+CREATE TABLE Orders (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  userId INT,
+  guestId INT,
+  totalPrice DECIMAL(10, 2), 
+  FOREIGN KEY (userId) REFERENCES Users(id),
+  FOREIGN KEY (guestId) REFERENCES Guest(id)
+); 
 
-DROP TABLE IF EXISTS `guest`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `guest` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `firstName` varchar(255) NOT NULL,
-  `lastName` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL,
-  `city` varchar(255) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Table: OrderItems
+CREATE TABLE OrderItems (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  orderId INT,
+  productId INT,
+  quantity INT,
+  totalPrice DECIMAL(10, 2), 
+  FOREIGN KEY (orderId) REFERENCES Orders(id),
+  FOREIGN KEY (productId) REFERENCES Products(id)
+);
 
---
--- Dumping data for table `guest`
---
+INSERT INTO Category (Categ)
 
-LOCK TABLES `guest` WRITE;
-/*!40000 ALTER TABLE `guest` DISABLE KEYS */;
-/*!40000 ALTER TABLE `guest` ENABLE KEYS */;
-UNLOCK TABLES;
+VALUES
+  ('Skincare'),
+  ('Makeup'),
+  ('Haircare'),
+  ('Fragrance'),
+  ('Body Care');
+  
 
---
--- Table structure for table `orders`
---
+INSERT INTO Products (productName, descrip, quantity, price, idCategory)
+VALUES
+  ('Cleansing Foam', 'Gentle facial cleanser for daily use', 50, 14.99, 1),
+  ('Moisturizing Cream', 'Hydrating face cream for all skin types', 30, 29.99, 1),
+  ('Mascara', 'Volumizing and lengthening mascara', 40, 12.99, 2),
+  ('Lipstick', 'Long-lasting matte lipstick in a vibrant red shade', 20, 9.99, 2),
+  ('Shampoo', 'Nourishing shampoo for healthy and shiny hair', 50, 16.99, 3),
+  ('Conditioner', 'Deep conditioning treatment for smooth and silky hair', 45, 18.99, 3),
+  ('Perfume', 'Elegant floral fragrance for women', 25, 49.99, 4),
+  ('Face Mask', 'Revitalizing face mask with natural ingredients', 60, 7.99, 1),
+  ('Eyeshadow Palette', 'Palette with a range of neutral and vibrant eyeshadows', 15, 24.99, 2),
+  ('Hair Serum', 'Nourishing serum for frizz-free and manageable hair', 35, 21.99, 3),
+  ('Body Lotion', 'Moisturizing body lotion with a refreshing scent', 40, 12.99, 5),
+  ('Highlighter', 'Illuminating highlighter for a radiant glow', 30, 14.99, 2),
+  ('Facial Toner', 'Balancing toner to hydrate and refine skin', 55, 16.99, 1),
+  ('Eyeliner', 'Long-wearing eyeliner for precise application', 25, 8.99, 2),
+  ('Hair Mask', 'Intensive hair mask for deep conditioning and repair', 20, 19.99, 3),
+  ('Blush', 'Pigmented blush for a natural flush of color', 35, 11.99, 2),
+  ('Body Wash', 'Gentle body wash with a soothing fragrance', 40, 9.99, 5),
+  ('Facial Scrub', 'Exfoliating facial scrub to remove dead skin cells', 30, 13.99, 1),
+  ('Lip Balm', 'Hydrating lip balm for soft and smooth lips', 50, 4.99, 2),
+  ('Hand Cream', 'Nourishing hand cream for dry hands', 60, 8.99, 5);
 
-DROP TABLE IF EXISTS `orders`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `orders` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `userId` int DEFAULT NULL,
-  `guestId` int DEFAULT NULL,
-  `totalPrice` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `userId` (`userId`),
-  KEY `guestId` (`guestId`),
-  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`),
-  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`guestId`) REFERENCES `guest` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orders`
---
-
-LOCK TABLES `orders` WRITE;
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `products`
---
-
-DROP TABLE IF EXISTS `products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `products` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `productName` varchar(255) NOT NULL,
-  `descrip` text,
-  `quantity` int DEFAULT NULL,
-  `price` decimal(10,2) DEFAULT NULL,
-  `idCategory` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idCategory` (`idCategory`),
-  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`idCategory`) REFERENCES `category` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `products`
---
-
-LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'Cleansing Foam','Gentle facial cleanser for daily use',50,14.99,1),(2,'Moisturizing Cream','Hydrating face cream for all skin types',30,29.99,1),(3,'Mascara','Volumizing and lengthening mascara',40,12.99,2),(4,'Lipstick','Long-lasting matte lipstick in a vibrant red shade',20,9.99,2),(5,'Shampoo','Nourishing shampoo for healthy and shiny hair',50,16.99,3),(6,'Conditioner','Deep conditioning treatment for smooth and silky hair',45,18.99,3),(7,'Perfume','Elegant floral fragrance for women',25,49.99,4),(8,'Face Mask','Revitalizing face mask with natural ingredients',60,7.99,1),(9,'Eyeshadow Palette','Palette with a range of neutral and vibrant eyeshadows',15,24.99,2),(10,'Hair Serum','Nourishing serum for frizz-free and manageable hair',35,21.99,3),(11,'Body Lotion','Moisturizing body lotion with a refreshing scent',40,12.99,5),(12,'Highlighter','Illuminating highlighter for a radiant glow',30,14.99,2),(13,'Facial Toner','Balancing toner to hydrate and refine skin',55,16.99,1),(14,'Eyeliner','Long-wearing eyeliner for precise application',25,8.99,2),(15,'Hair Mask','Intensive hair mask for deep conditioning and repair',20,19.99,3),(16,'Blush','Pigmented blush for a natural flush of color',35,11.99,2),(17,'Body Wash','Gentle body wash with a soothing fragrance',40,9.99,5),(18,'Facial Scrub','Exfoliating facial scrub to remove dead skin cells',30,13.99,1),(19,'Lip Balm','Hydrating lip balm for soft and smooth lips',50,4.99,2),(20,'Hand Cream','Nourishing hand cream for dry hands',60,8.99,5);
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `firstName` varchar(255) NOT NULL,
-  `lastName` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL,
-  `city` varchar(255) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `points` int DEFAULT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-09-21 19:56:02
